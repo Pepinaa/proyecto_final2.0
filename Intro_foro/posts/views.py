0838d7154b28505d7be6ac0673cbc905bdcad2a9
Nxchoez <90395995+Nxchoez@users.noreply.github.com>
2023-10-25 20:58:11 -0300

    posts

--- Intro_foro/posts/views.py
+++ Intro_foro/posts/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 def posts(request):
-    post = Post.objects.all().order_by('-created_at')
+    post = Post.objects.all().order_by('-create_at')
     return render(request, 'posts/posts.html', {'posts': posts})
+
+def create_post(request):
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save()
+            return redirect('posts:posts')
+
+
+    return render(request, 'posts/create_post.html', {'form': form})
