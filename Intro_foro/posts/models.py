0b5553af8efa1b3a14db41c997b2313897b50393
Nxchoez <90395995+Nxchoez@users.noreply.github.com>
2023-10-24 23:00:48 -0300

    Posts

---
+++ Intro_foro/posts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    #imagen = models.ImageField()
+    likes = models.PositiveIntegerField(default=0, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    quote = models.TextField(null=True, blank=True)
+
+class Comment(models.Model):
+    text = models.TextField()
