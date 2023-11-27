d3e4130e7e9e0d8676e9368aab7c9d0cc4c147ab
Nxchoez <90395995+Nxchoez@users.noreply.github.com>
2023-11-27 16:48:56 -0300

    listo

--- Intro_foro/apps/network/models.py
+++ Intro_foro/apps/network/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     description = models.TextField()
     image = models.ImageField(
         upload_to='images/',
-        default='images/default.png'
+        default='images/a.png'
     )
     published = models.DateTimeField(default=timezone.now)
     likes = models.IntegerField()
